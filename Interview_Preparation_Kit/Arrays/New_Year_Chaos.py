0f27970334222b6757820026403333f9ba4612e7
AliceXJ <55777544+AliceXJ@users.noreply.github.com>
2019-09-30 17:17:10 +0800

    Update New_Year_Chaos.py

--- Interview_Preparation_Kit/Arrays/New_Year_Chaos.py
+++ Interview_Preparation_Kit/Arrays/New_Year_Chaos.py
@@ -10,11 +10,11 @@ import sys
 def minimumBribes(q):
     count = 0
     for i in range (0,len(q)):
-        if q [i] > i+3:               //can at most bribe 2 different ppl, means at most moving forward twice.
+        if q [i] > i+3:               #can at most bribe 2 different ppl, means at most moving forward twice.
             print("Too chaotic")
             return            
         else:
-            for j in range(max(0,q[i]-2),i):      //to reduce runtime, only choose to begin the loop(for j) from the max position where a larger number may appaears 
+            for j in range(max(0,q[i]-2),i):      #to reduce runtime, only choose to begin the loop(for j) from the max position where a larger number may appaears 
                 if q[j] > q[i]:
                     count += 1
     print (count)
